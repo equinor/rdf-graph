8659000b43257f0d9c2c3ceae625aa4838db0034
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2022-04-06 11:36:05 +0200

    fix gh pr

--- src/components/sparqlGraph/SparqlGraph.tsx
+++ src/components/sparqlGraph/SparqlGraph.tsx
@@ -1,6 +1,8 @@
 import { SparqlGraphProps } from './SparqlGraph.types';
 import { ChartInterface } from '../../interface/chart';
 
+import { UiEdge } from './uiEdge';
+
 export const SparqlGraph = ({ turtleString, layoutName }: SparqlGraphProps) => {
 	return <ChartInterface turtleString={turtleString} layoutName={layoutName} />;
 };
