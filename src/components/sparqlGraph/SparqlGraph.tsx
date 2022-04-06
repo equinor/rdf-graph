3bb750ee1b682a445fd036ceb43b6102b0fb814c
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2022-04-06 11:56:13 +0200

    test with snyk SARIF

--- src/components/sparqlGraph/SparqlGraph.tsx
+++ src/components/sparqlGraph/SparqlGraph.tsx
@@ -1,8 +1,6 @@
 import { SparqlGraphProps } from './SparqlGraph.types';
 import { ChartInterface } from '../../interface/chart';
 
-import { UiEdge } from './uiEdge';
-
 export const SparqlGraph = ({ turtleString, layoutName }: SparqlGraphProps) => {
 	return <ChartInterface turtleString={turtleString} layoutName={layoutName} />;
 };
