54d15fc9b4c4899c136fdc4ea90bdaf909c541c2
Dzianis <dzianis.makeichyk@gmail.com>
2022-04-08 12:52:58 +0200

    patch: create code-quality.yml (#61)

--- src/components/details/Details.stories.tsx
+++ src/components/details/Details.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import { Details } from '../details/Details';
+import { Details } from './Details';
 
 export default {
 	title: 'Details',
