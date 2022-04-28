1f27be7cd9cad460bb9417667856707440896ac4
Lorentz F. Barstad <29151133+lorentzfb@users.noreply.github.com>
2022-04-28 09:11:51 +0200

    Comment

--- src/symbol-api/svg-manipulation.ts
+++ src/symbol-api/svg-manipulation.ts
@@ -1,4 +1,4 @@
-/**  */
+/** Parse a SVG as a string to a SVGSVGElement */
 export function stringToSvgElement(svg: string): SVGSVGElement {
 	const doc = new DOMParser().parseFromString(svg, 'image/svg+xml');
 	return doc.lastChild as SVGSVGElement;
