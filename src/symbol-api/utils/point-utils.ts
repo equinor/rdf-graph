8281722b81a28aebdc43aec69088086c529a1bc6
Lorentz F. Barstad <29151133+lorentzfb@users.noreply.github.com>
2022-04-28 09:11:42 +0200

    Return new Point

--- src/symbol-api/utils/point-utils.ts
+++ src/symbol-api/utils/point-utils.ts
@@ -1,17 +1,12 @@
 import { Point } from '../types/Point';
 
+/** Rotates a Point (x,y) around orgio  */
 export function rotatePoint(point: Point, rotation: number): Point {
 	const theta = (rotation * Math.PI) / 180;
-	return {
-		x: point.x * Math.cos(theta) - point.y * Math.sin(theta),
-		y: point.y * Math.cos(theta) + point.x * Math.sin(theta),
-	};
+	return new Point(point.x * Math.cos(theta) - point.y * Math.sin(theta), point.y * Math.cos(theta) + point.x * Math.sin(theta));
 }
 
 /** Converts a Point with origo TopLeft to a point with origo CenterCenter */
 export function pointToCenterCenter(point: Point, xRng: number, yRng: number): Point {
-	return {
-		x: -xRng / 2 + point.x,
-		y: yRng / 2 - point.y,
-	};
+	return new Point(-xRng / 2 + point.x, yRng / 2 - point.y);
 }
