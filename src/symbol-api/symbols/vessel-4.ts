d40043499e17c6bc4cb42daee1198b01eaa122b1
Eirik Nordstrand <89242375+einorequinor@users.noreply.github.com>
2022-06-13 15:39:57 +0200

    added symbols for pnid and pfd (#159)

---
+++ src/symbol-api/symbols/vessel-4.ts
@@ -0,0 +1,49 @@
+import { NodeSymbolTemplate } from '../types/NodeSymbol';
+import { NodeSymbolConnector } from '../types/NodeSymbolConnector';
+
+const svg = `<svg width="96" height="282" viewBox="0 0 96 282" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="vessel-4">
+<g id="group1422-1">
+<g id="shape1408-2">
+<path id="Vector" d="M92.7144 29.1075L92.7144 252.552" stroke="black" stroke-miterlimit="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<g id="group1421-5">
+<g id="shape1406-6">
+<path id="Vector_2" d="M92.7153 252.499H3.25934" stroke="black" stroke-miterlimit="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<g id="shape1407-9">
+<path id="Vector_3" d="M92.7534 29.0565L3.29749 29.0565" stroke="black" stroke-miterlimit="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<g id="shape1409-12">
+<path id="Vector_4" d="M3.33677 29.0547L3.33677 252.499" stroke="black" stroke-miterlimit="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<g id="shape1413-15">
+<path id="Vector_5" d="M3.27197 28.5623C4.24184 25.535 6.10934 22.5555 8.76787 19.7942C11.4264 17.0328 14.8239 14.5435 18.7663 12.4685C22.7087 10.3935 27.1189 8.77335 31.745 7.70061C36.3711 6.62788 41.1225 6.12357 45.728 6.21644" stroke="black" stroke-miterlimit="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<g id="shape1414-18">
+<path id="Vector_6" d="M92.7293 28.562C90.0185 22.6888 83.544 17.0354 74.73 12.8451C65.916 8.65476 55.4842 6.2708 45.7292 6.21755" stroke="black" stroke-miterlimit="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<g id="shape1415-21">
+<path id="Vector_7" d="M92.7283 253.202C91.7584 256.229 89.8909 259.209 87.2323 261.97C84.5738 264.731 81.1763 267.221 77.2339 269.296C73.2915 271.371 68.8813 272.991 64.2552 274.063C59.6291 275.136 54.8776 275.641 50.2722 275.548" stroke="black" stroke-miterlimit="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<g id="shape1416-24">
+<path id="Vector_8" d="M3.27183 253.203C5.98263 259.076 12.4571 264.73 21.2712 268.92C30.0852 273.11 40.517 275.494 50.272 275.547" stroke="black" stroke-miterlimit="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+</g>
+</g>
+</g>
+</svg>
+`;
+
+export const Vessel_4: NodeSymbolTemplate = {
+	id: 'vessel-4',
+	svg: svg,
+	height: 282,
+	width: 96,
+	connectors: [
+		new NodeSymbolConnector('c1', { x: 3.34003, y: 57.5 }),
+		new NodeSymbolConnector('c2', { x: 3.34003, y: 175.5 }),
+		new NodeSymbolConnector('c3', { x: 48.5, y: 275.7 }),
+		new NodeSymbolConnector('c4', { x: 48.5, y: 6.29999 }),
+	],
+};
---
+++ src/symbol-api/symbols/vg-o.ts
@@ -0,0 +1,25 @@
+import { NodeSymbolTemplate } from '../types/NodeSymbol';
+import { NodeSymbolConnector } from '../types/NodeSymbolConnector';
+
+const svg = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg>
+<svg width="98" height="50" viewBox="0 0 98 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="vg-o">
+<g id="shape1413-1">
+<path id="Vector" fill-rule="evenodd" clip-rule="evenodd" d="M0.999999 49.0958L49 25.0958L0.999999 1.0958V49.0958Z" stroke="black" stroke-miterlimit="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<g id="shape1414-3">
+<path id="Vector_2" fill-rule="evenodd" clip-rule="evenodd" d="M97 1.0958L49 25.0958L97 49.0958V1.0958Z" stroke="black" stroke-miterlimit="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<g id="shape1422-5">
+<path id="Vector_3" d="M49 1.0958V49.5958" stroke="black" stroke-miterlimit="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+</g>
+</svg>`;
+
+export const VG_O: NodeSymbolTemplate = {
+	id: 'vg-o',
+	svg: svg,
+	height: 50,
+	width: 98,
+	connectors: [new NodeSymbolConnector('c1', { x: 1, y: 25 }), new NodeSymbolConnector('c2', { x: 97, y: 25 })],
+};
