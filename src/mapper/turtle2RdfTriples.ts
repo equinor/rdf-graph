36b93ebf53bb60818db34e49086ede0f63c458fa
Eirik Alme Norstrand <einor@equinor.com>
2022-07-06 11:56:05 +0200

    expand namepaces in turtle2rdftripes

--- src/mapper/turtle2RdfTriples.ts
+++ src/mapper/turtle2RdfTriples.ts
@@ -1,2 +1,27 @@
-import { Parser } from 'n3';
-export const turtle2RdfTriples = (turtle: string) => new Parser({ format: 'Turtle' }).parse(turtle);
+import { Parser, DataFactory, PrefixCallback } from 'n3';
+export function turtle2RdfTriples(turtle: string) {
+	const prefixes = new Map<string, string>();
+	const pc: PrefixCallback = (prefix, node) => {
+		prefixes.set(prefix, node.value);
+	};
+	const namedNode: typeof DataFactory.namedNode = (value) => {
+		const cpos = value.indexOf(':');
+		const pfx = value.substring(0, cpos);
+		let namespace: string | undefined;
+		switch (pfx) {
+			case 'http':
+			case 'https':
+				return DataFactory.namedNode(value);
+			case '':
+				namespace = prefixes.get('base');
+				break;
+			default:
+				namespace = prefixes.get(pfx);
+				break;
+		}
+		if (!namespace) return DataFactory.namedNode(value);
+		return DataFactory.namedNode((namespace + value.substring(cpos + 1)) as typeof value);
+	};
+	const parser = new Parser({ format: 'Turtle', factory: { ...DataFactory, namedNode } });
+	return parser.parse(turtle, undefined, pc);
+}
