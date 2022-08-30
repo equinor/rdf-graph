3710bdec7967b20faca73c30d2e6a7fb068fa530
Martin Tofteberg Ulvesæter <48928133+toftalisk@users.noreply.github.com>
2022-08-30 11:03:27 +0200

    feat: predicate props becomes edge props (#211)
    * Add feat predicate props becomes edge props
    
    We use metadata to store properties related to types and predicates.
    These are not directly graph properties. Still, we used to yield them
    and the gui implementations was responsible for interpreting it and
    change properties to all elements with the corresponding types or all edges
    that used the corresponding predicate. This responsibility should be
    moved to core and in this commit we solve the issue for predicates
    (types are still remaining work)
    
    An example is if a predicate is used twice resulting in two edges in the
    corresponding graph. A new triple adding properties to the predicate
    (using the predicate as subject) will result in 2 properties yielded to
    the gui implementation. This brings us one step closer to the dream of
    our gui implementation only implementing an interface with simple
    methods like:
    
    AddNode(<id>)
    AddEdge(<id>)
    AddProperty(<node- or edgeid>, <key>, <value>)
    RemoveNode(<id>)
    ...
    
    Added a new test strategy in src/core/tests/patchGraph/testUtils.ts. We
    use simplified assertions to match against the actual assertions yielded
    from patchGraph to check that we receive the edge-, node- or
    property-assertions in the right order. We do not inspect more content
    opening some possibilities for bugs, but these tests with this
    abstraction has proven to be useful.
    
    Modified cygraph storywrapper to further tests the new functionality.
    Has not yet been tested in the other GUI implementation other than not
    breaking existing stories.
    
    * Change order of object IRI and predicate handling
    
    Useful to group object IRI and object literal handling together. Order
    in patchGraph is now:
    
        * Subject handling
        * Object handling (first literal then iris)
        * Predicate handling

--- src/core/tests/patchGraph/graphState.test.tsx
+++ src/core/tests/patchGraph/graphState.test.tsx
@@ -3,12 +3,12 @@ import { patchGraph } from '../../state/patchGraph';
 import { termToId } from 'n3';
 import * as P from '../../mapper/predicates';
 import { SymbolKey } from '../../../symbol-api';
-import { emptyGraph, svgWithConnectorQuads, toPatch } from './testUtils';
+import { emptyGraph, svgWithConnectorQuads, toAddPatch } from './testUtils';
 
 describe('patchGraph', () => {
 	test('normal svg', () => {
 		const quads = svgWithConnectorQuads('c1');
-		const res = patchGraph(emptyGraph(), toPatch(quads));
+		const res = patchGraph(emptyGraph(), toAddPatch(quads));
 		for (const _ of res.graphPatch) {
 		}
 		for (const q of quads) {
@@ -39,7 +39,7 @@ describe('patchGraph', () => {
 	});
 
 	test('Without existing connector', () => {
-		const res = patchGraph(emptyGraph(), toPatch(svgWithConnectorQuads("I don't exist in Seperator_1")));
+		const res = patchGraph(emptyGraph(), toAddPatch(svgWithConnectorQuads("I don't exist in Seperator_1")));
 		for (const _ of res.graphPatch) {
 		}
 		expect(res.graphState.nodeIndex.get('C')!.relativePosition!).toMatchObject({ x: 0, y: 0 });
