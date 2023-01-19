ba9b1288a5bd3fed53755f32f250f4e9f3a1ec25
Lorentz F. Barstad <29151133+lorentzfb@users.noreply.github.com>
2023-01-19 15:33:41 +0100

    update f3d comp

--- src/f3d/RdfF3dGraph.tsx
+++ src/f3d/RdfF3dGraph.tsx
@@ -1,29 +1,16 @@
-import { useEffect, useRef, useState } from 'react';
-
-import { GraphPatch, GraphState } from '../core/types/core';
-import { RdfGraphProps } from '../core/types/ui';
-import { patchGraphState } from '../core/patch';
-
+import { useEffect, useRef } from 'react';
 import ForceGraph3D, { ForceGraph3DInstance } from '3d-force-graph';
+
+import { GraphPatch } from '../core/types/core';
+import { RdfGraphProps } from '../core/types/ui';
 
 export type RdfF3dGraphProps = RdfGraphProps<Event> & {
 	onF3dInit?: (f3d: ForceGraph3DInstance) => void;
 };
 
-export const RdfF3dGraph = ({
-	customGraphPatches,
-	rdfPatches,
-	symbolProvider,
-	onGraphStateChanged,
-}: RdfF3dGraphProps) => {
+export const RdfF3dGraph = ({ graphPatches }: RdfF3dGraphProps) => {
 	const divRef = useRef<HTMLDivElement>(null);
 	const f3dGraphInstance = useRef<ForceGraph3DInstance>();
-
-	const [graphState, setGraphState] = useState<GraphState>({
-		nodeStore: {},
-		predicateNodeStore: {},
-		edgeStore: {},
-	});
 
 	useEffect(() => {
 		const f3dGraph = ForceGraph3D();
@@ -40,43 +27,35 @@ export const RdfF3dGraph = ({
 		}
 	}, []);
 
-	useEffect(() => {
-		const patchGraphResult = patchGraphState(graphState, rdfPatches, { symbolProvider });
-		setGraphState(patchGraphResult.graphState);
-
-		if (onGraphStateChanged) {
-			onGraphStateChanged({ ...patchGraphResult.graphState });
-		}
-		applyPatches(patchGraphResult.graphPatches);
-	}, [rdfPatches]);
-
-	useEffect(() => {
-		applyPatches(customGraphPatches);
-	}, [customGraphPatches]);
-
-	useEffect(() => {
-		if (!f3dGraphInstance.current) return;
-
-		const nodes = Object.keys(graphState.nodeStore).map((k) => {
-			return { id: k };
-		});
-		const links = Object.keys(graphState.edgeStore).map((k) => {
-			return {
-				id: k,
-				source: graphState.edgeStore[k].sourceId,
-				target: graphState.edgeStore[k].targetId,
-			};
-		});
-
-		f3dGraphInstance.current.graphData({
-			nodes: nodes,
-			links: links,
-		});
-	}, [graphState]);
-
 	const applyPatches = (_patches: GraphPatch[]) => {
 		if (!f3dGraphInstance.current) return;
+
+		// TODO: Martin...
 	};
+
+	useEffect(() => {
+		applyPatches(graphPatches);
+	}, [graphPatches]);
+
+	// useEffect(() => {
+	// 	if (!f3dGraphInstance.current) return;
+
+	// 	const nodes = Object.keys(graphState.nodeStore).map((k) => {
+	// 		return { id: k };
+	// 	});
+	// 	const links = Object.keys(graphState.edgeStore).map((k) => {
+	// 		return {
+	// 			id: k,
+	// 			source: graphState.edgeStore[k].sourceId,
+	// 			target: graphState.edgeStore[k].targetId,
+	// 		};
+	// 	});
+
+	// 	f3dGraphInstance.current.graphData({
+	// 		nodes: nodes,
+	// 		links: links,
+	// 	});
+	// }, [graphState]);
 
 	return <div ref={divRef}></div>;
 };
