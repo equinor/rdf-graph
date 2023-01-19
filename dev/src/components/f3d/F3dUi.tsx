b8eb338bae34d9257908f494ac36379e419729e5
toftalisk <martin.tofteberg@gmail.com>
2023-01-19 14:08:20 +0100

    chore: some force graph 3d fixes

--- dev/src/components/f3d/F3dUi.tsx
+++ dev/src/components/f3d/F3dUi.tsx
@@ -1,9 +1,14 @@
 import { RdfF3dGraph } from '@rdf-graph/f3d/RdfF3dGraph';
-import { GraphState } from '@rdf-graph/core';
+import { GraphState, UiSymbol } from '@rdf-graph/core';
 import { GraphSelection, useGraphContext } from '../../context/GraphContext';
+import { getConnectorSymbolAdvanced, SymbolLibraryKey } from '../../symbol-api';
 
 export const F3dUi = () => {
 	const { graphContext, dispatch } = useGraphContext();
+
+	function fg3SymbolProvider(id: string, rotation?: number) {
+		return getConnectorSymbolAdvanced(id as SymbolLibraryKey, { rotation: rotation }) as UiSymbol;
+	}
 
 	const graphStateChangedHandler: (state: GraphState) => void = (state) => {
 		dispatch({
@@ -24,6 +29,7 @@ export const F3dUi = () => {
 			<RdfF3dGraph
 				rdfPatches={graphContext.rdfPatches}
 				customGraphPatches={graphContext.customPatches}
+				symbolProvider={fg3SymbolProvider}
 				onGraphStateChanged={graphStateChangedHandler}
 				onGraphSelectionChanged={graphSelectionChangedHandler}
 			/>
--- src/f3d/RdfF3dGraph.tsx
+++ src/f3d/RdfF3dGraph.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useRef, useState } from 'react';
 
-import { GraphPatch, GraphState } from '../core/types/core';
+import { GraphPatch, GraphState, KnownPropKey, PatchProp } from '../core/types/core';
 import { RdfGraphProps } from '../core/types/ui';
 import { patchGraphState } from '../core/patch';
 
 import ForceGraph3D, { ForceGraph3DInstance } from '3d-force-graph';
+import { CustomGraphStore } from './custom.types';
 
 export type RdfF3dGraphProps = RdfGraphProps<Event> & {
 	onF3dInit?: (f3d: ForceGraph3DInstance) => void;
@@ -14,15 +15,21 @@ export const RdfF3dGraph = ({
 	customGraphPatches,
 	rdfPatches,
 	symbolProvider,
-	onGraphStateChanged,
+	onGraphSelectionChanged,
 }: RdfF3dGraphProps) => {
 	const divRef = useRef<HTMLDivElement>(null);
 	const f3dGraphInstance = useRef<ForceGraph3DInstance>();
+	const [initialized, setInitialized] = useState(false);
 
 	const [graphState, setGraphState] = useState<GraphState>({
 		nodeStore: {},
 		predicateNodeStore: {},
 		edgeStore: {},
+	});
+
+	const [fg3Model, setFg3Model] = useState<CustomGraphStore>({
+		nodes: [],
+		links: [],
 	});
 
 	useEffect(() => {
@@ -34,19 +41,21 @@ export const RdfF3dGraph = ({
 		const element = divRef.current;
 
 		if (element) {
-			f3dGraphInstance.current = f3dGraph(element);
+			const instance = f3dGraph(element);
+			instance.onNodeClick((n, _e) =>
+				onGraphSelectionChanged({ nodes: [(n as { id: string }).id], edges: [] })
+			);
+			f3dGraphInstance.current = instance;
 		} else {
 			console.error('Fatal error during f3dgraph startup');
 		}
+		setInitialized(true);
 	}, []);
 
 	useEffect(() => {
+		if (!initialized) return;
 		const patchGraphResult = patchGraphState(graphState, rdfPatches, { symbolProvider });
 		setGraphState(patchGraphResult.graphState);
-
-		if (onGraphStateChanged) {
-			onGraphStateChanged({ ...patchGraphResult.graphState });
-		}
 		applyPatches(patchGraphResult.graphPatches);
 	}, [rdfPatches]);
 
@@ -57,26 +66,77 @@ export const RdfF3dGraph = ({
 	useEffect(() => {
 		if (!f3dGraphInstance.current) return;
 
-		const nodes = Object.keys(graphState.nodeStore).map((k) => {
-			return { id: k };
-		});
-		const links = Object.keys(graphState.edgeStore).map((k) => {
-			return {
-				id: k,
-				source: graphState.edgeStore[k].sourceId,
-				target: graphState.edgeStore[k].targetId,
-			};
-		});
+		if (fg3Model) {
+			// force graph 3d will ruin our model if we pass it
+			const copy = JSON.parse(JSON.stringify(fg3Model));
+			f3dGraphInstance.current.graphData(copy);
+		}
+	}, [fg3Model]);
 
-		f3dGraphInstance.current.graphData({
-			nodes: nodes,
-			links: links,
-		});
-	}, [graphState]);
-
-	const applyPatches = (_patches: GraphPatch[]) => {
+	const applyPatches = (patches: GraphPatch[]) => {
 		if (!f3dGraphInstance.current) return;
+		for (const patch of patches) {
+			if (patch.action === 'remove') continue;
+			const content = patch.content;
+			switch (content.type) {
+				case 'node':
+					setFg3Model((store) => {
+						return { ...store, nodes: [...store.nodes, { id: content.id }] };
+					});
+					break;
+				case 'edge':
+					setFg3Model((store) => {
+						return {
+							...store,
+							links: [
+								...store.links,
+								{ id: content.id, source: content.sourceId, target: content.targetId },
+							],
+						};
+					});
+					break;
+				case 'property':
+					setFg3Model((store) => {
+						return { ...store, nodes: addProp(content.id, content.prop, store.nodes) };
+					});
+					setFg3Model((store) => {
+						return { ...store, links: addProp(content.id, content.prop, store.links) };
+					});
+					break;
+			}
+		}
 	};
+
+	const propMap = (key: KnownPropKey) => {
+		switch (key) {
+			case 'fill':
+				return 'color';
+			case 'stroke':
+				return 'color';
+			case 'label':
+				return 'name';
+		}
+		return key;
+	};
+
+	function addProp<T extends { id: string }>(id: string, prop: PatchProp, store: T[]): T[] {
+		const index = store.findIndex((n) => n.id === id);
+		if (index === -1) {
+			return store;
+		}
+		const oldElement = store[index];
+
+		const fg3Key = prop.type === 'custom' ? prop.key : propMap(prop.key);
+
+		const newElement = {
+			...oldElement,
+			[fg3Key]: prop.value,
+		};
+
+		const newStore = [...store.slice(0, index), newElement, ...store.slice(index + 1)];
+
+		return newStore;
+	}
 
 	return <div ref={divRef}></div>;
 };
