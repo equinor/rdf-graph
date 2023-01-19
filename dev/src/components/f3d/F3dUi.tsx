5e79097dbf86f0ba38745c49eb89c63bbc5c7249
Martin Tofteberg Ulvesæter <48928133+toftalisk@users.noreply.github.com>
2023-01-19 15:27:58 +0100

    chore: some force graph 3d fixes (#317)

--- dev/src/components/f3d/F3dUi.tsx
+++ dev/src/components/f3d/F3dUi.tsx
@@ -1,9 +1,14 @@
-import { RdfF3dGraph, GraphState } from '@rdf-graph';
+import { RdfF3dGraph, GraphState, UiSymbol} from '@rdf-graph';
 
 import { GraphSelection, useGraphContext } from '../../context/GraphContext';
+import { getConnectorSymbolAdvanced, SymbolLibraryKey } from '../../symbol-api';
 
 export const F3dUi = () => {
 	const { graphContext, dispatch } = useGraphContext();
+
+	function fg3SymbolProvider(id: string, rotation?: number) {
+		return getConnectorSymbolAdvanced(id as SymbolLibraryKey, { rotation: rotation }) as UiSymbol;
+	}
 
 	const graphStateChangedHandler: (state: GraphState) => void = (state) => {
 		dispatch({
@@ -24,6 +29,7 @@ export const F3dUi = () => {
 			<RdfF3dGraph
 				rdfPatches={graphContext.rdfPatches}
 				customGraphPatches={graphContext.customPatches}
+				symbolProvider={fg3SymbolProvider}
 				onGraphStateChanged={graphStateChangedHandler}
 				onGraphSelectionChanged={graphSelectionChangedHandler}
 			/>
