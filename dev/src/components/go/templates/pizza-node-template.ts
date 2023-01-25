c024fcd5ddea9b2b8f0de49d40834c3f5ead9288
Lorentz F. Barstad <29151133+lorentzfb@users.noreply.github.com>
2023-01-25 12:49:54 +0100

    feat(dev): add pizza converter button

--- dev/src/components/go/templates/pizza-node-template.ts
+++ dev/src/components/go/templates/pizza-node-template.ts
@@ -1,19 +1,17 @@
 import * as go from 'gojs';
 
-export function createPizzaNodeTemplate(
-	clickHandler?: (e: go.InputEvent, thisObj: go.GraphObject) => void
-): go.Node {
+export function createPizzaNodeTemplate(): go.Node {
 	return new go.Node(go.Panel.Vertical, {
 		resizable: false,
-		click: clickHandler,
+		click: () => console.info('You clicked on a pizza'),
 	})
 		.add(new go.Picture('/pizza.svg', { desiredSize: new go.Size(100, 100) }))
 		.add(
-			new go.TextBlock('<NoLabel>', {
+			new go.TextBlock('<NoLabel: use custom prop "pizzaName">', {
 				margin: 8,
 				wrap: go.TextBlock.WrapFit,
 				stroke: 'orange',
 				font: '20px "Comic Sans MS"',
-			}).bind('text', 'label')
+			}).bind('text', 'custom_pizzaName')
 		);
 }
