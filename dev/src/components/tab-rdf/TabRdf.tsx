49adaba92b94e53f39d606117dab4ff0638ddfe7
Lorentz F. Barstad <29151133+lorentzfb@users.noreply.github.com>
2023-01-30 13:48:56 +0100

    feat: add quad to string export

--- dev/src/components/tab-rdf/TabRdf.tsx
+++ dev/src/components/tab-rdf/TabRdf.tsx
@@ -1,19 +1,50 @@
-import { Table } from '@equinor/eds-core-react';
+import { Autocomplete, Button, Table, Typography } from '@equinor/eds-core-react';
 
-import { MimeFormat, Prefixes, Quad, Writer } from 'n3';
+import { Prefixes, Quad, Writer } from 'n3';
 import { useEffect, useState } from 'react';
 
 import { useGraphContext } from '../../context/GraphContext';
 import { RdfIri } from '../rdf-iri/RdfIri';
 import { getRdfPrefixes } from '../../rdf/rdf-utils';
-//import css from './TabGraphState.module.css';
+import css from './TabRdf.module.css';
 
-function _quadsToRdf({}: { quads: Quad[]; format?: MimeFormat }) {}
+export async function copyTextToClipboard(text: string) {
+	if ('clipboard' in navigator) {
+		return await navigator.clipboard.writeText(text);
+	} else {
+		return document.execCommand('copy', true, text);
+	}
+}
+
+const exportFormatOptions = ['Turtle', 'N-Triples'] as const;
+
+function exportQuadsAsString(quads: Quad[], format: (typeof exportFormatOptions)[number]) {
+	const pref: Prefixes<string> = {};
+
+	for (const [k, v] of getRdfPrefixes()) {
+		pref[k] = v;
+	}
+
+	const writer = new Writer({ prefixes: pref, format: format });
+	writer.addQuads(quads);
+
+	let res = '';
+
+	writer.end((err, result) => {
+		res = result;
+	});
+
+	return res;
+}
 
 export const TabRdf = () => {
 	const { graphContext } = useGraphContext();
 
 	const [quads, setQuads] = useState<Quad[]>([]);
+	const [exportFormat, setExportFormat] = useState<(typeof exportFormatOptions)[number]>(
+		exportFormatOptions[0]
+	);
+	const [isCopied, setIsCopied] = useState<boolean>(false);
 
 	useEffect(() => {
 		if (!graphContext.quadStore) return;
@@ -23,54 +54,67 @@ export const TabRdf = () => {
 			quadItems.push(q);
 		}
 		setQuads(quadItems);
-		exportAsString();
 	}, [graphContext.quadStore]);
 
-	function exportAsString() {
-		if (!graphContext.quadStore) return;
-
-		const pref: Prefixes<string> = {};
-
-		for (const [k, v] of getRdfPrefixes()) {
-			pref[k] = v;
+	async function exportToClipboardHandler() {
+		if (quads.length === 0) return;
+		const res = exportQuadsAsString(quads, exportFormat);
+		try {
+			await copyTextToClipboard(res);
+		} catch (error) {
+			return;
 		}
-
-		const writer = new Writer({ prefixes: pref, format: '' });
-
-		for (const q of graphContext.quadStore) {
-			writer.addQuad(q);
-		}
-
-		writer.end((error, result) => console.log(result));
+		console.log(res);
+		setIsCopied(true);
+		setTimeout(() => {
+			setIsCopied(false);
+		}, 1500);
 	}
 
 	return (
-		<Table>
-			<Table.Head>
-				<Table.Row>
-					<Table.Cell>#</Table.Cell>
-					<Table.Cell>Subject</Table.Cell>
-					<Table.Cell>Predicate</Table.Cell>
-					<Table.Cell>Object</Table.Cell>
-				</Table.Row>
-			</Table.Head>
-
-			<Table.Body>
-				{quads.map((p, i) => (
-					<Table.Row key={i}>
-						<Table.Cell>{i + 1}</Table.Cell>
-						<Table.Cell>
-							<RdfIri iri={p.subject.value} singleLine={false} />
-						</Table.Cell>
-						<Table.Cell>
-							<RdfIri iri={p.predicate.value} singleLine={false} />
-						</Table.Cell>
-						<Table.Cell>
-							<RdfIri iri={p.object.value} singleLine={false} />
-						</Table.Cell>
+		<div>
+			<div className={css.export}>
+				<Autocomplete
+					className={css.exportFormatOpt}
+					label="Export Format"
+					initialSelectedOptions={[exportFormatOptions[0]]}
+					options={[...exportFormatOptions]}
+					onOptionsChange={(c) => {
+						if (c.selectedItems[0]) setExportFormat(c.selectedItems[0]);
+					}}
+				/>
+				<Button variant="contained" onClick={exportToClipboardHandler}>
+					Export to Clipboard
+				</Button>
+				<Typography variant="h6">{isCopied ? 'Copied!' : ''}</Typography>
+			</div>
+			<Table>
+				<Table.Head>
+					<Table.Row>
+						<Table.Cell>#</Table.Cell>
+						<Table.Cell>Subject</Table.Cell>
+						<Table.Cell>Predicate</Table.Cell>
+						<Table.Cell>Object</Table.Cell>
 					</Table.Row>
-				))}
-			</Table.Body>
-		</Table>
+				</Table.Head>
+
+				<Table.Body>
+					{quads.map((p, i) => (
+						<Table.Row key={i}>
+							<Table.Cell>{i + 1}</Table.Cell>
+							<Table.Cell>
+								<RdfIri iri={p.subject.value} singleLine={false} />
+							</Table.Cell>
+							<Table.Cell>
+								<RdfIri iri={p.predicate.value} singleLine={false} />
+							</Table.Cell>
+							<Table.Cell>
+								<RdfIri iri={p.object.value} singleLine={false} />
+							</Table.Cell>
+						</Table.Row>
+					))}
+				</Table.Body>
+			</Table>
+		</div>
 	);
 };
