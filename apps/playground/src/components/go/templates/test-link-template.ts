ed7e0aab9392b9d02545f332a1739580463d012a
Martin Ulvesæter <mulv@equinor.com>
2023-05-04 14:22:49 +0200

    feat: support gojs edge templates
    Was a bit hard to figure out how to change edge categories in gojs.
    
    This thread https://forum.nwoods.com/t/dynamically-change-link-category-with-multiple-link-templates/11464/7
    + this jsfiddled https://codepen.io/jhardy/pen/xmgJgw?editors=1010
      helped
    
    Main idea is that we can use linkdata accable from `(diagram.model as go.GraphLinksModel).findLinkDataForKey(propPatch.id)` for updating props on link, but
    when we want to change category we need an acutal link object accessable
    from either select `myDiagram.selection.first();` or
    `diagram.findLinkForKey(propPatch.id)`

---
+++ apps/playground/src/components/go/templates/test-link-template.ts
@@ -0,0 +1,10 @@
+import * as go from 'gojs';
+
+export function createTestLinkTemplate(): go.Link {
+	return (
+		new go.Link()
+			.add(
+				new go.Shape({ stroke: 'cyan', strokeWidth: 20 })
+			)
+	);
+}
